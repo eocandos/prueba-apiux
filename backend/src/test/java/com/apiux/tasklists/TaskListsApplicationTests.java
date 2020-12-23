035e0e4d121a95c53ff07df0fe5ffe8b35596fee
Erick <eaocandos@gmail.com>
2020-12-23 16:49:10 -0500

    Proyecto spring boot vacio

---
+++ backend/src/test/java/com/apiux/tasklists/TaskListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiux.tasklists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
