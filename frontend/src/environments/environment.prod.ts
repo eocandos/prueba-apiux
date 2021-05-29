8f953eea0c3524acd4af11fc140c70cbc483a835
eocando <eocando@bancolombia.com.co>
2021-05-28 21:35:15 -0500

    Adecuacion CrossOrigin y conexion en cuenta AWS

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true
 };
 
-export const apiUrl = '//localhost:8080/api';
+export const apiUrl = '//ec2-18-217-92-102.us-east-2.compute.amazonaws.com:8080/api';
+// export const apiUrl = '//localhost:8080/api';
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
 };
 
 export const apiUrl = '//localhost:8080/api';
+// export const apiUrl = '//ec2-18-217-92-102.us-east-2.compute.amazonaws.com:8080/api';
 
 /*
  * For easier debugging in development mode, you can import the following file
